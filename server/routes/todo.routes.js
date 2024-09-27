2389b3d41ba215bb67aeb4cfb9734ec415ffa30f
Abhishek k <bobbyyyyy.16@gmail.com>
2024-09-27 21:00:02 +0530

    completed Routes

---
+++ server/routes/todo.routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const {
+  createTodo,
+  getAllTodos,
+  updateTodo,
+  deleteTodo,
+} = require("../controllers/todo.controller");
+const authMiddleware = require("../middlewares/auth.middlewares.js");
+const router = express.Router();
+
+// Protect task routes with authMiddleware
+router.use(authMiddleware);
+
+// Task routes
+router.post("/", createTodo);
+router.get("/", getAllTodos);
+router.put("/:taskId", updateTodo);
+router.delete("/:taskId", deleteTodo);
+
+module.exports = router;
